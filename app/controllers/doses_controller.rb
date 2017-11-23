384288db3e1032ce607637978541d5572809c0d9
francomuoio <franco.59171@gmail.com>
2017-11-23 14:34:31 +0100

    ajout partie validation rake

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,32 @@
+class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:new, :create]
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    if @dose.save
+      redirect_to cocktail_path(params[:cocktail_id])
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+end
