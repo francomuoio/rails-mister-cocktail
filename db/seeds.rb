274034c03ad306e1ee69a22567b100d60b1d0ce7
francomuoio <franco.59171@gmail.com>
2017-11-24 10:35:26 +0100

    ajout json

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
 
-10.times do |restaurant|
-restaurant =  Ingredient.create!(
-    name: Faker::Name.name
-  )
+require 'json'
+require 'open-uri'
+
+
+
+require 'json'
+require 'open-uri'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+ingredients = JSON.parse(user_serialized)
+
+
+ingredients[‘drinks’].each do |element|
+   Ingredient.create(name: element['strIngredient1'])
 end
